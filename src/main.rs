b1ea1d348097e00d0b5790cee88c5401a07f20e2
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2017-02-02 16:22:07 -0800

    use InstrType struct to describe different instructions tpyes

--- src/main.rs
+++ src/main.rs
@@ -19,46 +19,11 @@
 use std::fmt;
 use std::ops;
 
-// TODO: remove redundancy in instruction definition with the use of macros
-//       or compiler plugin in the spirit of:
-//       https://crates.io/crates/enum_primitive
-const ADD: u16    = 0b00001;
-const SUB: u16    = 0b10000;
-const STORE: u16  = 0b00010;
-const CLEAR: u16  = 0b00011;
-const OR: u16     = 0b00000;
-const AND: u16    = 0b00100;
-const SHIFTR: u16 = 0b00101;
-const SHIFTL: u16 = 0b00110;
-const BGE: u16    = 0b00111;
-const BLT: u16    = 0b01000;
-const END: u16    = 0b01010;
-
-fn op_to_str(op: u16) -> &'static str{
-	match op {
-		ADD    => "ADD",
-		SUB    => "SUB",
-		STORE  => "STORE",
-		CLEAR  => "CLEAR",
-		OR     => "OR",
-		AND    => "AND",
-		SHIFTR => "SHIFTR",
-		SHIFTL => "SHIFTL",
-		BGE    => "BGE",
-		BLT    => "BLT",
-		END    => "END",
-		_ => panic!("Unknown op code ({:05b})", op),
-	}
-}
-
 const MAX_MEM: usize = 2048;
-
 fn mask(bits: u32) -> u16 { (1 << bits) - 1 }
 
+// Machine State
 struct Regs { acc: Integer, pc: u16 }
-struct Instruction { w: u16 }
-#[derive(Copy, Clone)]
-struct Integer { w: u16}
 
 impl Regs {
 	fn new(pc: u16) -> Regs {
@@ -71,6 +36,80 @@ impl fmt::Display for Regs {
 	}
 }
 
+// Instruction Definitions
+struct InstrType { name: &'static str, opcode: u16, exec: fn(u16, Regs, &mut[u16]) -> Regs }
+
+// unfortunately the synthax for generating function pointers on the fly
+// is somewhat difficult to read for now, this should be fixed once the
+// following change is merged: https://github.com/rust-lang/rfcs/pull/1558
+const ADD:    InstrType = InstrType { name: "ADD",    opcode: 0b00001,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc + Integer::load(n, mem), pc: old.pc + 1}
+	} exec } };
+
+const SUB:    InstrType = InstrType { name: "SUB",    opcode: 0b10000,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc - Integer::load(n, mem), pc: old.pc + 1}
+	} exec } };
+
+const STORE:  InstrType = InstrType { name: "STORE",  opcode: 0b00010,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	{ mem[n as usize] = old.acc.w; Regs { acc: old.acc, pc: old.pc + 1} }
+	} exec } };
+
+const CLEAR:  InstrType = InstrType { name: "CLEAR",  opcode: 0b00011,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: Integer::new(0), pc: old.pc + 1}
+	} exec } };
+
+const OR:     InstrType = InstrType { name: "OR",     opcode: 0b00000,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc | Integer::load(n, mem), pc: old.pc + 1}
+	} exec } };
+
+const AND:    InstrType = InstrType { name: "AND",    opcode: 0b00100,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc & Integer::load(n, mem), pc: old.pc + 1}
+	} exec } };
+
+const SHIFTR: InstrType = InstrType { name: "SHIFTR", opcode: 0b00101,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc >> n, pc: old.pc + 1}
+	} exec } };
+
+const SHIFTL: InstrType = InstrType { name: "SHIFTL", opcode: 0b00110,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc << n, pc: old.pc + 1}
+	} exec } };
+
+const BGE:    InstrType = InstrType { name: "BGE",    opcode: 0b00111,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc, pc: if !old.acc.less_than_zero() { n } else { old.pc + 1} }
+	} exec } };
+
+const BLT:    InstrType = InstrType { name: "BLT",    opcode: 0b01000,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc, pc: if old.acc.less_than_zero() { n } else { old.pc + 1} }
+	} exec } };
+
+const END:    InstrType = InstrType { name: "END",    opcode: 0b01010,
+exec: { fn exec(n: u16, old: Regs, mem: &mut[u16]) -> Regs {
+	Regs { acc: old.acc, pc: old.pc} // TODO: signal halted
+	} exec } };
+
+
+static INSTRUCTION_TYPES: [InstrType; 11] =
+	[ADD, SUB, STORE, CLEAR, OR, AND, SHIFTR, SHIFTL, BGE, BLT, END];
+
+impl InstrType {
+	fn new(&self, n: u16) -> Instruction {
+		Instruction::new(self.opcode, n)
+	}
+}
+
+// Instruction represents an actual instruction in the program
+struct Instruction { w: u16 }
+
 impl Instruction {
 	fn new(op: u16, n: u16) -> Instruction {
 		Instruction { w: (op << 11) | (n & mask(11)) }
@@ -78,26 +117,46 @@ impl Instruction {
 	fn load(pc: u16, mem: &[u16]) -> Instruction {
 		Instruction { w: mem[pc as usize] }
 	}
-	fn n(&self) -> u16 { self.w & mask(11) }
-	fn op(&self) -> u16 { self.w >> 11 }
-	fn arg(&self, mem: &[u16]) -> Integer { Integer::load(self.n(), mem) }
+	fn get_type(&self) -> &'static InstrType {
+		let opcode = self.op();
+		for tt in INSTRUCTION_TYPES.iter() {
+			if tt.opcode == opcode {
+				return tt;
+			}
+		}
+		panic!("Unknown op code ({:05b})", opcode)
+	}
+	fn n(&self) -> u16 {
+		return self.w & mask(11)
+	}
+	fn op(&self) -> u16 {
+		return self.w >> 11
+	}
+	fn exec(&self, old: Regs, mem: &mut[u16]) -> Regs {
+		(self.get_type().exec)(self.n(), old, mem)
+	}
 }
 
 impl fmt::Display for Instruction {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{} {}", op_to_str(self.op()), self.n())
+		let tt = self.get_type();
+		write!(f, "{} {}", tt.name, self.n())
 	}
 }
 
+
+// 16bit Integer format
+#[derive(Copy, Clone)]
+struct Integer { w: u16}
 impl Integer {
 	fn new(ii: i16) -> Integer {
-		let abs_val = (ii.abs() as u16) & ((1<<15) - 1);
+		let abs_val = (ii.abs() as u16) & mask(15);
 		if ii >= 0 { Integer { w: abs_val } }
 		else { Integer { w: (1<<15) | abs_val } }
 	}
 	fn load(index: u16, mem: &[u16]) -> Integer { Integer { w: mem[index as usize] } }
 	fn is_positive(&self) -> bool { self.w & (1 << 15) == 0}
-	fn abs(&self) -> u16 { self.w & ((1<<15) - 1) }
+	fn abs(&self) -> u16 { self.w & mask(15) }
 	fn sign(&self) -> u16 { self.w & (1<<15) }
 	fn less_than_zero(&self) -> bool { !self.is_positive() && self.abs() != 0 }
 }
@@ -113,7 +172,7 @@ impl ops::Add for Integer {
 	fn add(self, other: Integer) -> Integer {
 		if self.is_positive() == other.is_positive() {
 			let result = self.abs() as u32 + other.abs() as u32;
-			if result > ((1<<15) - 1) {
+			if result > mask(15) as u32 {
 				panic!("Overflow detected trying to execute {} + {}", self, other);
 			}
 			Integer { w: result as u16 | self.sign() }
@@ -164,46 +223,16 @@ impl ops::Shl<u16> for Integer {
 	}
 }
 
-fn add(n: u16)   -> u16 { Instruction::new(ADD, n).w }
-fn sub(n: u16)   -> u16 { Instruction::new(SUB, n).w }
-fn store(n: u16) -> u16 { Instruction::new(STORE, n).w }
-fn clear()       -> u16 { Instruction::new(CLEAR, 0).w }
-fn or(n: u16)    -> u16 { Instruction::new(OR, n).w }
-fn and(n: u16)   -> u16 { Instruction::new(AND, n).w }
-fn end()         -> u16 { Instruction::new(END, 0).w }
-fn con(n: i16)   -> u16 { Integer::new(n).w }
-
-
-fn exec(old: Regs, mem: &mut[u16]) -> Regs {
-	let instr = Instruction::load(old.pc, mem);
-	match instr.op() {
-		// TODO: implement correct under/overflow behavior
-		ADD    => Regs { acc: old.acc + instr.arg(mem), pc: old.pc + 1},
-		SUB    => Regs { acc: old.acc - instr.arg(mem), pc: old.pc + 1},
-		STORE  => { mem[instr.n() as usize] = old.acc.w;
-		          Regs { acc: old.acc, pc: old.pc + 1} },
-		CLEAR  => Regs { acc: Integer::new(0), pc: old.pc + 1},
-		OR     => Regs { acc: old.acc | instr.arg(mem), pc: old.pc + 1},
-		AND    => Regs { acc: old.acc & instr.arg(mem), pc: old.pc + 1},
-		SHIFTR => Regs { acc: old.acc >> instr.n(), pc: old.pc + 1},
-		SHIFTL => Regs { acc: old.acc << instr.n(), pc: old.pc + 1},
-		BGE    => Regs { acc: old.acc,
-		                 pc: if !old.acc.less_than_zero() { instr.n() } else { old.pc + 1} },
-		BLT    => Regs { acc: old.acc,
-		                 pc: if old.acc.less_than_zero() { instr.n() } else { old.pc + 1} },
-		END    => Regs { acc: old.acc, pc: old.pc}, // TODO: signal halted
-		_ => panic!("Unknown op code ({:05b})", instr.op()),
-	}
-}
-
 fn run(pc: u16, mem: &mut[u16]) -> Regs {
 	let mut regs = Regs::new(pc);
-	while Instruction::load(regs.pc, mem).op() != END {
-		print!("{:04}: {}", regs.pc, Instruction::load(regs.pc, mem));
+	while {
+		let instr = Instruction::load(regs.pc, mem);
+		print!("{:04}: {}", regs.pc, instr);
 		let acc_old = regs.acc;
-		regs = exec(regs, mem);
+		regs = instr.exec(regs, mem);
 		println!("\tacc: {:>6} => {:>6}", acc_old, regs.acc);
-	}
+		instr.op() != END.opcode
+	}{}
 	regs
 }
 
@@ -216,6 +245,15 @@ fn print_mem(mem: &[u16]) {
 		ii = ii + 1;
 	}
 }
+
+fn add(n: u16)   -> u16 { ADD.new(n).w }
+fn sub(n: u16)   -> u16 { SUB.new(n).w }
+fn store(n: u16) -> u16 { STORE.new(n).w }
+fn clear()       -> u16 { CLEAR.new(0).w }
+fn or(n: u16)    -> u16 { OR.new(n).w }
+fn and(n: u16)   -> u16 { AND.new(n).w }
+fn end()         -> u16 { END.new(0).w }
+fn con(n: i16)   -> u16 { Integer::new(n).w }
 
 fn main() {
 	let mut mem: [u16; 7] = [
